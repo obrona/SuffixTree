8718238910b905eb010fba285223562c1e7db1da
obrona <tlim8772@gmail.com>
2024-12-30 17:30:05 +0800

    delete failed code

--- SuffixTree.cpp
+++ SuffixTree.cpp
@@ -1,136 +1,181 @@
+// The correct one
+// Finally I got a CORRECT working implementation of Ukkonen algorithm
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// -1 means it is the end variable #
-struct Edge {
-    int l, r = -1;
-    int idx; // next node idx
-};
-
 struct Node {
-    int suffix_link = 0; // just point to root as default
-    unordered_map<char, Edge> edges;
+    int l, r = -1;
+    Node *slink = nullptr;
+    unordered_map<char, Node*> edges;
+
+    Node() {}
+
+    Node(int l, int r): l(l), r(r) {}
+
+    int get_len(int end_ptr) {
+        return (r == -1) ? end_ptr - l + 1 : r - l + 1;
+    }
+
+    void print(char c, int len) {
+        cout << string(len, ' ') << c << " " << l << " " << r << endl;
+        for (const auto& [key, node] : edges) {
+            node->print(key, len + 1);
+        }
+
+    }
 };
-
-// ukkonen algorithm
-// invariants
-// 1. once a leaf, always a leaf, because we never insert at leaf, because leaf contains a suffix i.e [l, end], cannot extemd
-// 2. suffix link is to a string path with 1 fewer char from the front
-// 3. all nodes at lvl 1 and below, except leafs have suffix link (because we never after a leaf node)
-//    because suppose we have a path abc-(ccd, xxy) (split path), then we have a suffix abcccd... and abcxxy...
-//    then we have a suffix bccd... and bcxxy...
-//    that means path bc must split
-//    only exception is if node is at lvl 1 and edge contains 1 char only 
-
-// suffix link for nodes at lvl 1 is to root (implicitly)
-// root is always at idx 0
 
 struct SuffixTree {
-    string s;
+    string str;
+    int end_ptr = -1;
 
-    int active_node_idx;
-    char active_edge_char;
-    int active_len;
-    int remainder;
-    int end_ptr;
+    Node *root;
+    Node *active_node;
+    char active_char = '#'; // only '#' if active_len is 0, because we dont have a char
+    int active_len = 0;
+    int remainder = 0;
 
-    vector<Node> nodes;
-
-    SuffixTree(string& s): s(s), nodes(1, Node()) {}
-
-
-    int new_node() {
-        int idx = nodes.size();
-        nodes.emplace_back();
-        return idx;
+    SuffixTree(string& s): str(s), root(new Node(0, 0)), active_node(root) {
+        process();
     }
 
-    Node& get_node(int idx) {
-        return nodes[idx];
-    }
+    // move active node.
+    // l is the starting index of str, with active_len the length of the substring we are referencing
+    // we do not need to worry about exceeding a leaf node, this is not possible
+    // as active is the like the prefix of some suffix we are matching, and the pos to match cannot exceed end_ptr
+    void walk_down(int l) {
+        if (active_len == 0) return;
+        
+        while (active_len > 0) {
+            Node *next = active_node->edges[active_char];
+            int len = next->get_len(end_ptr);
 
-    // the substring pointed to starts at l and has alen char, so from [l, l + alen - 1]
-    void shift_active(int l, int alen) {
-        while (1) {
-            Node& node = get_node(active_node_idx);
-            Edge& edge = node.edges[active_edge_char];
-            int len = (edge.r == -1) ? end_ptr - edge.l + 1 : edge.r - edge.l + 1;
-
-            if (len > alen) break;
-            active_node_idx = edge.idx;
-            active_edge_char = s[l + len];
+            if (len > active_len) break;
+            active_node = next;
             active_len -= len;
+            active_char = (active_len == 0) ? '#' : str[l + len];
+            l += len;
+            
+            
         }
     }
 
-
-    // check whether we can just update active
-    // case 1: active_len == 0
-    // case 2: active_len > 0
-    bool update_active(int ptr) {
-        Node& node = get_node(active_node_idx);
-        
+    // check if we can match str[ptr]
+    bool update_active(int ptr, Node *prev) {
+        char c = str[ptr];
         if (active_len == 0) {
-            if (node.edges.count(s[ptr]) == 0) return false;
-            active_edge_char = s[ptr];
-            active_len ++;
-        } else {
-            char c = s[node.edges[active_edge_char].l + active_len];
-            if (c != s[ptr]) return false;
-            active_len ++;
-        }
-    }
+            if (prev != nullptr) prev->slink = active_node; // we found a path to the node of 1 char removed suffix
+            if (active_node->edges.count(c) == 0) return false;
 
-    // have to insert node, returns idx of the new node that is needed for suffix link
-    // case 1. new node needed does not split an edge (e.g active_len == 0), then only 1 node created
-    // case 2. need to split an edge, 2 nodes created, have to return the correct node idx needed for suffix link   
-    int insert_node(int ptr) {
-        Node& active_node = get_node(active_node_idx);
-        Edge& active_edge = active_node.edges[active_edge_char];
-        
-        
-        if (active_len == 0) {
-            int idx = new_node();
-            active_node.edges[s[ptr]] = {ptr, -1, idx};
-            return idx;
-        } else {
-            int node1_idx = new_node();
-            int node2_idx = new_node();
-            
-            Node &n1 = get_node(node1_idx), &n2 = get_node(node2_idx);
-            n1.edges[s[ptr]] = {ptr, -1, node2_idx};
-            
-            
-            int r1 = active_edge.l + active_len - 1;
-            int r2 = r1 + 1;
-            
-            n1.edges[s[r2]] = {r2, active_edge.r, active_edge.idx};
-            active_edge.r = r1;
-            active_edge.idx = node1_idx;
 
-            return node1_idx;
-        }
-    }
-
-    void update(int ptr) {
-        end_ptr ++;
-        remainder ++;
-
-        int prev_idx = 0;
-        while (remainder > 0) {
-            if (update_active(ptr)) break;
-
-            int new_node_idx = insert_node(ptr);
-            
-            // insert at root
-            if (active_node_idx == 0) {
-                active_edge_char = s[get_node(active_node_idx).edges[active_edge_char].l + 1];
-                active_len --;
-                remainder --;
+            Node *edge = active_node->edges[c];
+            active_char = c;
+            active_len = 1;
+            if (edge->get_len(end_ptr) == 1) {
+                active_node = edge;
+                active_len = 0;
             }
 
+        } else {
+            Node *edge = active_node->edges[active_char];
+            if (str[edge->l + active_len] != c) return false; // will end here, cannot match, as path must diverge
+                                                              // consider abcdeey
+                                                              //           bcdeex x is the char we looking for
+                                                              // but we must have bcdeey 
+            
+            active_len ++;
+            if (edge->get_len(end_ptr) == active_len) {
+                active_node = edge;
+                active_len = 0;
+            }
 
         }
-
+        
+        //walk_down(active_node->edges[active_char]->l);
+        return true;
     }
+
+
+    Node* insert(int ptr, Node *to_link) {
+        if (active_len == 0) {
+            if (to_link != nullptr) to_link->slink = active_node;
+            Node *n = new Node(ptr, -1);
+            active_node->edges[str[ptr]] = n;
+            return n;
+        } else {
+            Node *edge = active_node->edges[active_char];
+            int l = edge->l, r1 = l + active_len - 1, r2 = r1 + 1;
+            
+            Node *n1 = new Node(l, r1), *n2 = new Node(ptr, -1);
+            n1->edges[str[ptr]] = n2;
+            n1->edges[str[r2]] = edge;
+            edge->l = r2;
+            active_node->edges[active_char] = n1;
+            if (to_link != nullptr) to_link->slink = n1; // only link if we split an edge, we do not link leafs
+            return n1;
+        }
+    }
+
+    // if active_node path is only 1 char, dont have suffix link, invariant maintained
+    // as next insertion is from root
+    void process() {
+        for (int i = 0; i < str.size(); i ++) {
+            end_ptr ++;
+            remainder ++;
+
+            
+            Node *prev_inserted = nullptr;
+            while (remainder > 0) {
+                if (update_active(i, prev_inserted)) break;
+
+                int prev_l = (active_len == 0) ? -1 : active_node->edges[active_char]->l;
+                prev_inserted = insert(i, prev_inserted);
+                if (active_node == root) {
+                    active_len = max(0, active_len - 1);
+                    active_char = (active_len == 0) ? '#' : str[prev_l + 1];
+                    walk_down(prev_l + 1);
+                } else {
+                    active_node = (active_node->slink == nullptr) ? root : active_node->slink;
+                    walk_down(prev_l);
+                }
+                
+                remainder --;
+                
+                
+               
+            }
+        }
+    }
+
+    int count_repeated_substrings(Node *node) {
+        if (node->edges.size() == 0) return 0;
+
+        int sum = (node == root) ? 0 : node->get_len(end_ptr);
+        for (const auto& [c, next] : node->edges) sum += count_repeated_substrings(next);
+
+        return sum;
+    }
+
+    void print_tree() {
+        //cout << root->edges.size() << endl;
+        root->print('#', 0);
+    }
+
+    void print_active() {
+        cout << active_node << endl;
+        cout << active_char << endl;
+        cout << active_len << endl;
+        cout << remainder << endl;
+    }
+
+    
 };
+int main() {
+    string s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa$";
+    SuffixTree st(s);
+    st.print_tree();
+    st.print_active();
+    //cout << st.count_repeated_substrings(st.root) << endl;
+}
--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    cout << sizeof(int) << endl;
-    cout << sizeof(int&) << endl;
-    cout << sizeof(int*) << endl;
-}
